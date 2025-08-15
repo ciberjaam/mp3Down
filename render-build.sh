dcaebff80e9906417884b8c574b05ce1f1ac6bc3
ciberjaam <124893809+ciberjaam@users.noreply.github.com>
2025-08-15 11:46:03 -0400

    Add files via upload

--- render-build.sh
+++ render-build.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
+
+# Actualizar pip
+pip install --upgrade pip
+
+# Instalar ffmpeg
 apt-get update && apt-get install -y ffmpeg
+
+# Instalar todas las dependencias incluyendo gunicorn
 pip install -r requirements.txt
